03b6f37c557fce401c763fe807281647296ad634
dev-pedroduarte <upedrolucas10@gmail.com>
2024-11-18 10:00:15 -0300

    Corrige validações e ajustes nas funções fatorial, fibonacci e ehPrimo

--- src/math.js
+++ src/math.js
@@ -1,15 +1,40 @@
-function fatorial() {
-
+// Função fatorial
+function fatorial(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n < 0) {
+    throw new Error("Não existe fatorial de número negativo");
+  }
+  return n === 0 ? 1 : n * fatorial(n - 1);
 }
-function fibonacci() {
 
+// Função fibonacci
+function fibonacci(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 0) {
+    throw new Error("Não existe fibonacci de número menor ou igual a zero");
+  }
+  return n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 }
-function ehPrimo() {
 
+// Função ehPrimo
+function ehPrimo(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 1) {
+    throw new Error("Não número primo de número menor ou igual a 1");
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-module.exports = {
-  fatorial,
-  fibonacci,
-  ehPrimo
-}
+module.exports = { fatorial, fibonacci, ehPrimo };
+
